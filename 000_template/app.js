9dae0af495917877d2f4713d729c63e8f5a51524
dima <shifting.planes@gmail.com>
2016-08-29 16:21:09 -0400

    basic template 000 added

---
+++ 000_template/app.js
@@ -0,0 +1,7 @@
+//This is the most basic template for working with P5 javascript library
+function setup() {
+  createCanvas(300, 300);
+  //http://p5js.org/reference/#/p5/createCanvas
+  rect(50, 50, 80, 80);
+  //http://p5js.org/reference/#p5/rect
+}
