60a9ec21bbf0824dbee5de734e4848a7f6a1187d
dima <shifting.planes@gmail.com>
2016-09-20 14:50:03 -0400

    added networked examples

---
+++ 011_network_w_sockets/server.js
@@ -0,0 +1,28 @@
+//a very very simple server
+
+var express = require('express');
+var app = express();
+var server = app.listen(8081, function () {
+  console.log("Shazam! listening on port 8081");
+})
+var io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+io.sockets.on('connection', function (socket) {
+  console.log("Client ID"+socket.id+" connected");
+
+  app.get('/trigger_1', function(request,response){
+    // response.json({"status":"success"});
+    response.sendStatus(200);
+    console.log("trigger 1 pressed");
+    socket.emit('trigger_1');
+  })
+
+  app.get('/trigger_2', function(request,response){
+    // response.json({"status":"success"});
+    response.sendStatus(200);
+    console.log("trigger 2 pressed");
+    socket.emit('trigger_2');
+  })
+})
