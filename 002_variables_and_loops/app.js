dfd97a07851c45058e0734d65d6e71606355f99b
Dima Strakovsky <Digi-D@users.noreply.github.com>
2018-01-23 13:07:17 -0500

    Update app.js

--- 002_variables_and_loops/app.js
+++ 002_variables_and_loops/app.js
@@ -1,5 +1,4 @@
-//Introducing Colors and Line weights
-//color channels are going to be represented by numbers from 0 to 255 = 8bit values
+//Introducing Variables and Loops
 function setup() {
   createCanvas(600, 600);
   background(255,0,0,255); //this is a color in RGBA colorspace : red, green, blue and alpha (opacity)
