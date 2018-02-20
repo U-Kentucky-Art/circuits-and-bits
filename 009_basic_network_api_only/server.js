94cf03ea25b22a3075487d7178757b83fb4a123b
Dima Strakovsky <Digi-D@users.noreply.github.com>
2018-02-20 09:22:24 -0500

    Update server.js

--- 009_basic_network_api_only/server.js
+++ 009_basic_network_api_only/server.js
@@ -1,4 +1,4 @@
-//a very very simple server
+//a very very simple API example
 
 var express = require('express');
 var app = express();
@@ -6,23 +6,23 @@ var app = express();
 //here, we are going to create custom routes
 app.get('/trigger_1', function(request,response){
   // response.json({"status":"success"});
-  response.sendStatus(200);
+  response.status(200);
   console.log("trigger 1 pressed");
 })
 
 app.get('/trigger_2', function(request,response){
   // response.json({"status":"success"});
-  response.sendStatus(200);
+  response.status(200);
   console.log("trigger 2 pressed");
 })
 
 app.get('/', function(request,response){
   // response.json({"status":"success"});
   //console.log(request);
-  response.send(200, "waiting for your command, master");
+  response.status(200).send("waiting for your command, master");
   console.log("standby, awaiting command");
 })
 
-app.listen(8081, function () {
+app.listen(process.env.PORT || 8081, function () {
   console.log("Shazam!")
 })
